61e04f62c61c460bbb8e6280773b75a8f9e5bf09
Giovanna Revito <giovannarev.roz@gmail.com>
2025-11-20 21:56:37 -0300

    mais

---
+++ src/main/resources/db/migration/V2__usersinsert.sql
@@ -0,0 +1,45 @@
+-- Hash para Fiap@2025: $2a$10$B00K4pXo2J9qZ5bN1sS0H02.t/F5D7zP6R8uW7xY3vG2uA1sO0H0
+-- Hash para Fiap@2026: $2a$10$Q7W6cZ9vP4tY3sR2qX1w.e4vG0fH9bC8dE7uV0yL6xM5nO4i0jK3
+
+
+-- 1. USUÁRIO ADMINISTRADOR (gioadmin)
+INSERT INTO usuario (
+    nome_usuario,
+    senha_usuario,
+    area_atual,
+    area_interesse,
+    objetivo_carreira,
+    nivel_experiencia,
+    role
+) VALUES (
+    'gioadmin',
+    '$2a$12$RuA2uKskkSHqu/v5LbyefOCcsm1D.WBygGKFU5xkhPL8XBIGCXOuO',
+    'Administração de Sistemas',
+    'Segurança e Cloud',
+    'Compliance e LGPD',
+    'Sênior',
+    'ROLE_ADMIN'
+);
+
+-- 2. USUÁRIO COMUM (kaianuser)
+INSERT INTO usuario (
+    nome_usuario,
+    senha_usuario,
+    area_atual,
+    area_interesse,
+    objetivo_carreira,
+    nivel_experiencia,
+    role
+) VALUES (
+    'kaianuser',
+    '$2a$12$YNfFHj9mzt5L8Ovmssje0.XgyLbhOmlC4zQpAbJ.EAOcwGH82ErqO',
+    'Desenvolvimento Java',
+    'Inteligência Artificial',
+    'Especialista em Back-end',
+    'Júnior',
+    'ROLE_USER'
+);
+
+-- Insere um registro base de competência para evitar FK errors
+INSERT INTO competencia (nome_competencia, categoria_competencia, descricao_competencia)
+VALUES ('Programação Java', 'Hard Skill', 'Conhecimento em ecossistema Java e Spring Boot.');
--- src/main/resources/db/migration/V3__updatesenha.sql
+++
@@ -1,2 +0,0 @@
-UPDATE usuario SET senha_usuario = '$2a$10$DlZkCMSavStTKtOmOqtowONYwnyNKJybaHYRkgTyMbg9B/.rZBmxK' WHERE nome_usuario = 'giovanna';
-UPDATE usuario SET senha_usuario = '$2a$10$0i89DVjdf2Utoc6t8qGUSO6pqWcXumKj9BflqKBnFqszOwnwzzkoS' WHERE nome_usuario = 'admin';
